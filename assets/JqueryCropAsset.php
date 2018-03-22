a816b58ed447d9dee02161aabcf0504abbfabdf7
Dmitry Karpov <d.karpov@cherinfo.ru>
2018-03-22 14:26:12 +0300

    Bower alias

--- assets/JqueryCropAsset.php
+++ assets/JqueryCropAsset.php
@@ -6,7 +6,7 @@ use yii\web\AssetBundle;
 
 class JqueryCropAsset extends AssetBundle
 {
-	public $sourcePath ='@vendor/bower/jcrop';
+	public $sourcePath ='@vendor/bower-asset/jcrop';
 	public $js = ['js/Jcrop.min.js'];
 	public $css = ['css/Jcrop.min.css'];
 	public $depends = [
